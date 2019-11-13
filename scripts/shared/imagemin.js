20773318bb0e4004b7ce25fb3dad61a4f915a661
javifm86 <javifm86@gmail.com>
2019-11-13 12:41:07 +0100

    Update script for watch images including new folder for svg

--- scripts/shared/imagemin.js
+++ scripts/shared/imagemin.js
@@ -25,7 +25,7 @@ module.exports.runImagemin = (source, destiny) => {
             }),
             imageminGifsicle(),
             imageminSvgo({
-                plugins: [{ removeViewBox: false }]
+                plugins: [{ removeViewBox: false, removeUnknownsAndDefaults: false }]
             })
         ]
     });
