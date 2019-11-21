8c512750c41227d41df3e8f7634020c7de55c2bd
javifm86 <javifm86@gmail.com>
2019-11-21 13:24:09 +0100

    Move prefers-dark-mode plugin to separate file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-const postcss = require('postcss');
 // https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 module.exports = {
     theme: {
@@ -36,54 +35,21 @@ module.exports = {
     variants: {
         backgroundColor: ['responsive', 'hover', 'focus', 'dark', 'dark:hover', 'dark:focus'],
         borderColor: ['responsive', 'hover', 'focus', 'dark', 'dark:hover', 'dark:focus'],
-        textColor: ['responsive', 'hover', 'focus', 'group-hover', 'dark', 'dark:hover', 'dark:focus'],
+        textColor: [
+            'responsive',
+            'hover',
+            'focus',
+            'group-hover',
+            'dark',
+            'dark:hover',
+            'dark:focus',
+            'dark:group-hover',
+            'focus-within',
+            'dark:focus-within'
+        ],
         borderStyle: ['responsive', 'dark'],
         boxShadow: ['responsive', 'hover', 'focus', 'group-hover'],
         margin: ['responsive']
     },
-    plugins: [
-        function({ addVariant, e }) {
-            addVariant('dark', ({ container, separator }) => {
-                const variant = '';
-                return getSelector({ container, separator, variant });
-            });
-
-            addVariant('dark:hover', ({ container, separator }) => {
-                const variant = 'hover';
-                return getSelector({ container, separator, variant });
-            });
-
-            addVariant('dark:focus', ({ container, separator }) => {
-                const variant = 'focus';
-                return getSelector({ container, separator, variant });
-            });
-
-            addVariant('dark:active', ({ container, separator }) => {
-                const variant = 'active';
-                return getSelector({ container, separator, variant });
-            });
-
-            function getSelector({ container, separator, variant }) {
-                const supportsRule = postcss.atRule({ name: 'media', params: '(prefers-color-scheme: dark)' });
-                supportsRule.append(container.nodes);
-                container.append(supportsRule);
-                supportsRule.walkRules(rule => {
-                    switch (variant) {
-                        case 'focus':
-                            rule.selector = `.${e(`dark:focus${separator}${rule.selector.slice(1)}`)}:focus`;
-                            break;
-                        case 'hover':
-                            rule.selector = `.${e(`dark:hover${separator}${rule.selector.slice(1)}`)}:hover`;
-                            break;
-                        case 'active':
-                            rule.selector = `.${e(`dark:active${separator}${rule.selector.slice(1)}`)}:active`;
-                            break;
-                        default:
-                            rule.selector = `.${e(`dark${separator}${rule.selector.slice(1)}`)}`;
-                            break;
-                    }
-                });
-            }
-        }
-    ]
+    plugins: [require('./plugins/tailwindcss-prefers-dark-mode')()]
 };
